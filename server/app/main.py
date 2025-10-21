90b4fb1d76f21810c6ed74acc4edc163b5d41069
Adrian Mihalcea <adrian.mihalcea@tum.de>
2025-10-21 09:50:49 +0200

    attempt to fix openapi proxying for good

--- server/app/main.py
+++ server/app/main.py
@@ -66,6 +66,9 @@ def create_app():
 
     # Trust reverse proxy headers (X-Forwarded-For/Proto/Host/Port/Prefix)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1, x_prefix=1)
+    
+    # Configure Flask to handle reverse proxy properly
+    app.config['PREFERRED_URL_SCHEME'] = 'https'
 
     with app.app_context():
         init_database()
