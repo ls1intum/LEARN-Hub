a49f0ece91a21b0b4b77f107bc6f13db06b6ae98
Jonathan Ostertag <jonathan@ostertage.de>
2026-03-13 10:47:36 +0100

    chore: lint client

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -7,8 +7,8 @@ import { buttonVariants } from "./button-variants";
 
 export interface ButtonProps
   extends
-  React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
 
